0c6302cce4377315e76c258bbe4ebe8a04a28d2d
PizzaMust <103622901+PizzaMust@users.noreply.github.com>
2022-12-11 19:40:15 +0800

    update

--- docs/.vuepress/navbar/zh.ts
+++ docs/.vuepress/navbar/zh.ts
@@ -1,7 +1,15 @@
 import { navbar } from "vuepress-theme-hope";
 
 export const zh = navbar([
-  { text: "SDVX", icon: "gamepad", link: "/zh/guide/" },
+  { text: "SDVX", 
+    icon: "gamepad", 
+    link: "/zh/guide/"
+  },
+  {
+    text: "家用版SDVX",
+    icon: "gamepad",
+    link: "/zh/guide/eacsdvx/"
+  },
   {
     text: "向科乐美致敬",
     icon: "note",
--- docs/zh/guide/eacsdvx/README.md
+++ docs/zh/guide/eacsdvx/README.md
@@ -3,3 +3,5 @@ order: false
 ---
 
 # SDVX官方家用版教程
+
+<Badge text="TODO" />
